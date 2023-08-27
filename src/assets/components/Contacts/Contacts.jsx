8e801ebe13a1ca0a86baecf9cb487b562eaabddb
Bakhtiyor <odilovskijarik@gmail.com>
2023-08-27 23:46:34 +0500

    Contacts недоделан

--- src/assets/components/Contacts/Contacts.jsx
+++ src/assets/components/Contacts/Contacts.jsx
@@ -4,7 +4,13 @@ export default function Contacts() {
   return (
     <div id="contacts">
       <div className="contactsContainer">
-        
+        <h1 className="title">My contacts</h1>
+        <div className="social-container">
+            <a href="#" className="instagram social-card"><i className="icon fab fa-instagram"></i></a>
+            <a href="#" className="youtube social-card"><i className="icon fab fa-youtube"></i></a>
+            <a href="#" className="github social-card"><i className="icon fab fa-github"></i></a>
+            <a href="#" className="telegram social-card"><i className="icon fab fa-telegram"></i></a>
+        </div>
       </div>
     </div>
   );
